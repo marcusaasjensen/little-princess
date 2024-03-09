e7a8c5b595f47444667f35b6bffb1374ae5afca7
Marcus Aas Jensen <aasjensenm@gmail.com>
2024-03-09 17:57:48 +0100

    fix: save system saving.

--- Assets/Scripts/Managers/SaveData.cs
+++ Assets/Scripts/Managers/SaveData.cs
@@ -1,13 +1,33 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewSaveData", menuName = "ScriptableObjects/Save Data")]
+[Serializable]
 public class SaveData : ScriptableObject
 {
     [SerializeField] private string currentScene;
+    private Vector3 _playerPosition;
     
     public string CurrentScene
     {
         get => currentScene;
-        set => currentScene = value;
+        set
+        {
+            EditorUtility.SetDirty(this);
+            currentScene = value;
+            AssetDatabase.SaveAssets();
+        }
+    }
+    
+    public Vector3 PlayerPosition
+    {
+        get => _playerPosition;
+        set
+        {
+            EditorUtility.SetDirty(this);
+            _playerPosition = value;
+            AssetDatabase.SaveAssets();
+        }
     }
 }
